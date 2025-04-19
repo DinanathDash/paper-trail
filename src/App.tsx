a76db7d64353713aabc8e8f3f995279d09ac7ea3
Dinanath Dash <dashdinanath056@gmail.com>
2025-04-19 15:07:16 +0530

    feat: add Vercel Speed Insights integration and update dependencies
    - Added @vercel/speed-insights package to package.json
    - Updated next version to ^15.3.1
    - Integrated SpeedInsights component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import SemestersPage from "./pages/SemestersPage";
@@ -19,6 +20,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <div>
+        {/* ... */}
+        <SpeedInsights />
+      </div>
       <BrowserRouter>
         <Layout>
           <Routes>
