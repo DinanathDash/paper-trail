590c936ef2a1e975a1eef17a41cbb8201fef0758
Dinanath Dash <dashdinanath056@gmail.com>
2025-04-20 00:18:50 +0530

    feat: add @vercel/analytics and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import SemestersPage from "./pages/SemestersPage";
@@ -23,6 +24,8 @@ const App = () => (
       <div>
         {/* ... */}
         <SpeedInsights />
+        <Analytics />
+        {/* ... */}
       </div>
       <BrowserRouter>
         <Layout>
